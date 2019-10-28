b6406d37e63671a9f549f2a5400fbddbe5e19f49
Islam Mostafa <islamghany3@gmail.com>
2019-10-28 23:29:14 +0200

    Updates

---
+++ precache-manifest.10ca948ee963f760eaad65bc3d927e1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20a0f94cf203b9303736aeba8def2ab9",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "76f9c764157b162e4fe4",
+    "url": "/tic-tac-toe/static/css/main.22d910f3.chunk.css"
+  },
+  {
+    "revision": "08c46f9dc9215f5102be",
+    "url": "/tic-tac-toe/static/js/2.5d6224c1.chunk.js"
+  },
+  {
+    "revision": "76f9c764157b162e4fe4",
+    "url": "/tic-tac-toe/static/js/main.4f55f6a2.chunk.js"
+  },
+  {
+    "revision": "385629666264212be688",
+    "url": "/tic-tac-toe/static/js/runtime-main.9b00db00.js"
+  }
+]);
